03eb51f4cb21513898f57baf13625d1d1d725889
Virv <casarin.filippo17@gmail.com>
2020-04-15 12:18:53 +0200

    Bug fix

--- src/k-NN.cpp
+++ src/k-NN.cpp
@@ -5,9 +5,7 @@
 #include "dataset.h"
 using namespace std;
 
-// TODO: find and fix bug
-
-constexpr size_t K = 100;
+constexpr size_t K = 100; // 94.40%
 
 uint32_t distSQ(size_t i, size_t j) {
 	uint32_t d = 0, z;
@@ -22,12 +20,11 @@ uint8_t predict(size_t i) {
 	vector<pair<uint32_t, uint8_t>> D(train_labels.size());
 
 	for (size_t j = 0; j < train_labels.size(); j++)
-		D[i] = { distSQ(i, j), train_labels[j] };
+		D[j] = { distSQ(i, j), train_labels[j] };
 
 	sort(D.begin(), D.end());
 
 	int F[10] {};
-
 	for (size_t i = 0; i < min(K, D.size()); i++)
 		F[D[i].second] ++;
 
