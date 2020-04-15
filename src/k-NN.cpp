ad4194d990f4370350103353d04673f36375517f
Virv <casarin.filippo17@gmail.com>
2020-04-15 12:49:22 +0200

    Tested different K fo k-NN

--- src/k-NN.cpp
+++ src/k-NN.cpp
@@ -5,7 +5,22 @@
 #include "dataset.h"
 using namespace std;
 
-constexpr size_t K = 100; // 94.40%
+size_t K = 3;
+/* 1:   96.91%
+ * 2:   96.27%
+ * 3:   97.05%
+ * 4:   96.82%
+ * 5:   96.88%
+ * 6:   96.77%
+ * 7:   96.94%
+ * 8:   96.70%
+ * 9:   96.59%
+ * 10:  96.65%
+ * 20:  96.25%
+ * 30:  95.96%
+ * 40:  95.60%
+ * 100: 94.40%
+ */
 
 uint32_t distSQ(size_t i, size_t j) {
 	uint32_t d = 0, z;
@@ -31,12 +46,17 @@ uint8_t predict(size_t i) {
 	return max_element(F, F+10) - F;
 }
 
-int main() {
+int main(int argc, char** argv) {
+	if (1 < argc)
+		K = stoi(argv[1]);
+
 	load_dataset();
 
 	size_t C = 0;
 	for (size_t i = 0; i < test_labels.size(); i++) {
 		C += test_labels[i] == predict(i);
-		cout << C << " / " << i + 1 << endl;
+		if (i % 20 == 19)
+			cerr << C << " / " << i + 1 << '\n';
 	}
+	cout << K << ": " << C << " / " << test_labels.size() << endl;
 }
