4651055099307dd0d13821816527a817a45cd205
Filippo Casarin <casarin.filippo17@gmail.com>
2020-05-02 20:28:35 +0200

    Refactored dnn

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 # CC=g++ -std=c++17 -g -Og -Wall -Wextra -Iincludes -D_GLIBCXX_DEBUG -fsanitize=address
 CC=g++ -std=c++17 -Ofast -Wall -Wextra -Iincludes -DNDEBUG
+
 DATASET=$(addprefix dataset/,t10k-labels-idx1-ubyte train-images-idx3-ubyte train-labels-idx1-ubyte t10k-images-idx3-ubyte)
 BINARY=$(addprefix bin/,k-NN dnn)
 
 all: $(BINARY) $(DATASET)
 
 bin/k-NN: $(addprefix obj/,k-NN.o dataset.o)
-bin/dnn: $(addprefix obj/,dnn.o dataset.o)
+bin/dnn: $(addprefix obj/,dnn.o nn.o dataset.o)
 
 $(BINARY): bin/% : | bin/
 	$(CC) -o $@ $^
--- src/dnn.cpp
+++ src/dnn.cpp
@@ -1,128 +1,27 @@
-#include <assert.h>
-#include <math.h>
+#include <cstdlib>
+#include <ctime>
 
-#include <numeric>
 #include <algorithm>
-#include <array>
+#include <iostream>
+#include <numeric>
 #include <vector>
 
 #include <dataset.h>
+#include <nn.h>
 using namespace std;
-
-float sigmoid(float x) {
-	return 1 / (1 + exp(-x));
-}
-
-struct matrix {
-	float* data;
-	size_t N, M;
-
-	matrix(size_t N, size_t M) : N(N), M(M) {
-		data = new float[N * M];
-	}
-
-	~matrix() {
-		delete[] data;
-	}
-
-	inline float* operator[] (const size_t i) const {
-		assert(i < N);
-		return data + i * M;
-	}
-};
-
-struct NN {
-	vector<matrix> W;
-	vector<matrix> A;
-
-	NN(vector<int> D) {
-		W.reserve(D.size() - 1);
-		A.reserve(D.size() - 1);
-
-		for (size_t i = 1; i < D.size(); i++) {
-			W.emplace_back(D[i], D[i-1] + 1);
-			A.emplace_back(D[i], D[i-1] + 1);
-
-			for (size_t j = 0; j < W.back().N * W.back().M; j++) {
-				W.back().data[j] = (float)rand() / RAND_MAX * 2 - 1;
-				A.back().data[j] = 0;
-			}
-		}
-	}
-
-	vector<float> operator() (vector<float> I) {
-		for (matrix& m : W) {
-			assert(I.size() + 1 == m.M);
-			vector<float> O(m.N);
-
-			for (size_t i = 0; i < O.size(); i++) {
-				for (size_t j = 0; j < I.size(); j++)
-					O[i] += I[j] * m[i][j];
-				O[i] = sigmoid(O[i] + m[i][I.size()]);
-			}
-
-			I = move(O);
-		}
-
-		return I;
-	}
-
-	void backprop(const vector<float>& I, const vector<float>& O) {
-		vector<vector<float>> NN { I };
-
-		for (matrix& m : W) {
-			vector<float> I = NN.back();
-			vector<float>& O = NN.emplace_back(m.N);
-
-			assert(m.M);
-			assert(I.size() + 1);
-			assert(I.size() + 1 == m.M);
-
-			for (size_t i = 0; i < O.size(); i++) {
-				for (size_t j = 0; j < I.size(); j++)
-					O[i] += I[j] * m[i][j];
-				O[i] = sigmoid(O[i] + m[i][I.size()]);
-			}
-		}
-
-		vector<float> D(NN.back().size());
-		assert(D.size() == O.size());
-
-		for (size_t i = 0; i < D.size(); i++)
-			D[i] = 2 * (NN.back()[i] - O[i]);
-
-		for (size_t i = NN.size() - 2; i < NN.size(); i--) {
-			for (size_t j = 0; j < D.size(); j++)
-				D[j] *= NN[i + 1][j] * (1 - NN[i + 1][j]);
-
-			vector<float> T(W[i].M - 1);
-			for (size_t j = 0; j < D.size(); j++) {
-				for (size_t k = 0; k < T.size(); k++) {
-					T[k] += W[i][j][k] * D[j];
-					A[i][j][k] += NN[i][k] * D[j];
-				}
-				A[i][j][T.size()] += D[j];
-			}
-
-			D = move(T);
-		}
-	}
-
-	void apply() {
-		for (size_t i = 0; i < W.size(); i++)
-			for (size_t j = 0; j < W[i].N; j++)
-				for (size_t k = 0; k < W[i].M; k++) {
-					W[i][j][k] -= 0.01 * A[i][j][k];
-					A[i][j][k] = 0;
-				}
-	}
-};
 
 int main() {
 	srand(time(0));
 
 	load_dataset();
-	NN nn({28*28, 28, 28, 10});
+	NN nn {
+		new LayerLinear(28*28, 28),
+		new LayerSigmoid,
+		new LayerLinear(28   , 28),
+		new LayerSigmoid,
+		new LayerLinear(28   , 10),
+		new LayerSigmoid,
+	};
 
 	vector<int> S(train_labels.size());
 	iota(S.begin(), S.end(), 0);
@@ -130,11 +29,11 @@ int main() {
 	do {
 		random_shuffle(S.begin(), S.end());
 		for (size_t i = 0; i < S.size(); i++) {
-			vector<float> I(train_images[S[i]].begin(), train_images[S[i]].end());
-			for (float& f : I)
-				f /= 255;
+			vector<float> I(28*28);
+			for (size_t j = 0; j < I.size(); j++)
+				I[j] = train_images[S[i]][j] / 255.0f;
 
-			vector<float> O(10, 0);
+			vector<float> O(10);
 			O[train_labels[S[i]]] = 1;
 
 			nn.backprop(I, O);
@@ -145,9 +44,9 @@ int main() {
 
 		size_t C = 0;
 		for (size_t i = 0; i < test_labels.size(); i++) {
-			vector<float> I(test_images[i].begin(), test_images[i].end());
-			for (float& f : I)
-				f /= 255;
+			vector<float> I(28*28);
+			for (size_t j = 0; j < I.size(); j++)
+				I[j] = test_images[i][j] / 255.0f;
 
 			vector<float> O = nn(I);
 
